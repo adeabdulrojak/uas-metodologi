41d2d494977a6a2fce71eedc448e40049fa07936
adeabdulrojak <157685497+adeabdulrojak@users.noreply.github.com>
2024-01-25 15:15:33 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023
+        <a href="#">SMARTBRAIN</a>.</strong>
+    All rights reserved. | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0.0
+    </div>
+</footer>
